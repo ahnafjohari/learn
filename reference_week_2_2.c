04a1f17d1bea01abf5586826f56f76684361513e
Ahnaf Johari <ahnaf58@gmail.com>
2022-06-18 12:07:51 +0800

    Add gitignore for .exe and reference week 2

---
+++ reference_week_2_2.c
@@ -0,0 +1,27 @@
+/*
+Fundamental types sizeof operator
+Ahnaf Johari
+2022 June 18th
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int a = 5, b = 7, c = 6;  // declare and initialize
+    double average = 0.0;   // good practice
+    char ch = 'e';
+
+    printf("On my system\n");
+    
+    printf("int is %lu bytes.\n", sizeof(int));
+    printf("long int is %lu bytes.\n", sizeof(long int));
+
+    printf("char is %lu bytes.\n", sizeof(ch));
+
+    printf("float is %lu bytes.\n", sizeof(float));
+
+    printf("double is %lu bytes.\n", sizeof(double));
+    printf("loong double is %lu bytes.\n", sizeof(long double));
+
+    return 0;
+}
---
+++ reference_week_2_3.c
@@ -0,0 +1,18 @@
+/*
+Char in C
+A Fundamental Type
+Ahnaf Johari
+2022 June 18th
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    char c = 'a';
+
+    printf("c in ASCII is %d\n", c);
+    printf("c the character %c\n", c);
+    printf("Three consecutive chars are : %c%c%c \n", c, c+1, c+2);
+    printf("Three bell rings chars are : %c%c%c \n", '\a', '\a', '\a');
+    return 0;
+}
